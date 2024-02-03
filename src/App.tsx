d4cdadaa63c6f4e783c4a6e64b7b6a502bf63bc1
kynhix <kristopher.t.hicks@gmail.com>
2024-02-03 18:42:04 -0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+      </header>
+    </div>
+  );
+};
+
+export default App;
