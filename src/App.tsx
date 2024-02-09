970e643a7a8eed11680c847307815b02a071591e
kynhix <kristopher.t.hicks@gmail.com>
2024-02-09 18:46:26 -0500

    First version of room generation

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: Component = () => {
       case "unknown":
       case "secret":
       case "super-secret":
-        return 'bg-black cursor-pointer';
+        return 'bg-black cursor-pointer transition-colors hover:bg-neutral-800';
       case "treasure":
         return 'bg-yellow-500';
       default:
@@ -21,23 +21,25 @@ const App: Component = () => {
     }
   }
 
+  /** Called when a room is clicked */
   const onClickRoom = (room: Room) => {
     console.log(room);
   }
 
   return (
     <>
-      <div class='relative flex flex-col min-h-screen h-fit bg-neutral-900 justify-center items-center'>
-        <h1 class='absolute top-20 text-2xl font-bold text-white'>The Binding of Isaac: <span class='text-blue-400'>Secret Room Game</span></h1>
+      <div class='relative flex flex-col min-h-screen h-fit justify-center items-center'>
+        <h1 class='absolute top-20 text-4xl font-bold text-white'>The Binding of Isaac: <span class='text-blue-400'>Secret Room Game</span></h1>
         <div class='text-black'>
           <For each={floor().getRooms}>{(row) =>
             <div class='flex flex-row'>
               <For each={row}>{(room) => {
-                // Don't display undefined rooms
                 return room &&
                   <div class={`w-16 text-center aspect-square ${getRoomStyle(room)} ${room.isOrigin() && ' after:content-["Spawn"]'}`} onclick={() => onClickRoom(room)}>
-                    <div>{room.getDistanceToCenter()}</div>
+                    {/* <div>{room.getDistanceToCenter()}</div> */}
+                    <div>{floor().getUndefinedNeighbors(room.row, room.column)}</div>
                   </div>
+                  // Needed for spacing 
                   || <div class='w-16 text-center aspect-square'></div>
               }
               }</For>
@@ -45,7 +47,9 @@ const App: Component = () => {
           }</For>
         </div>
       </div>
-      <div class='h-24'></div>
+      <div class='h-24 p-4 flex flex-col justify-end'>
+        <div class="text-white">Made with SolidJS and TailwindCSS.</div>
+      </div>
     </>
   );
 };
--- src/game.ts
+++ src/game.ts
@@ -5,18 +5,7 @@ type Neighbors = {
   down?: Room;
   left?: Room;
   right?: Room;
-  length: number;
 };
-
-const forEachNeighbor = (neighbors: Neighbors, callback: (room: Room) => void) => {
-  if (neighbors.length == 0) {
-    return;
-  }
-  neighbors.up && callback(neighbors.up);
-  neighbors.down && callback(neighbors.down);
-  neighbors.left && callback(neighbors.left);
-  neighbors.right && callback(neighbors.right);
-}
 
 export class Room {
   private type: RoomType;
@@ -24,12 +13,14 @@ export class Room {
   private distToCenter: number;
   private position: { row: number, column: number };
   private deadEnd: boolean;
+  private connections: number;
 
   constructor(row: number, column: number, dist: number, deadEnd: boolean, type: RoomType = "empty") {
     this.type = type;
     this.position = { row: row, column: column };
     this.distToCenter = dist;
     this.deadEnd = deadEnd;
+    this.connections = 0;
   }
 
   get row() {
@@ -58,6 +49,10 @@ export class Room {
 
   isDeadEnd() {
     return this.deadEnd;
+  }
+
+  setDeadEnd(deadend: boolean) {
+    this.deadEnd = deadend;
   }
 
   isOrigin() {
@@ -104,19 +99,43 @@ export class Floor {
 
   private generateFloor(totalRooms: number) {
     // initialize spawn room
-    const rooms = [new Room(0, 0, 0, false, "unknown")];
-    this.roomsMatrix = [[rooms[0]]];
+    this.roomsArray = [new Room(0, 0, 0, false, "unknown")];
+    this.roomsMatrix = [[this.roomsArray[0]]];
     this.addRoom(0, 0, "empty");
     // running total of rooms left to create
     let roomsLeft = totalRooms;
 
     const createDeadEnd = (maxLength: number) => {
-      const curRoom = getRandomElement(this.roomsArray);
+      const rooms = this.roomsArray.filter((room) => this.getUndefinedNeighbors(room.row, room.column) && this.getRoomNeighbors(room.row, room.column) == 1);
+      if (!rooms.length) {
+        console.error("Could not find a valid branch coordinate.")
+        return;
+      }
+      let room = getRandomElement(rooms);
+      console.log(room)
       while (maxLength) {
+        this.addRoom(room.row, room.column, "empty");
+        const rooms = Object.values(this.getNeighbors(room.row, room.column)).filter((room) => this.getUndefinedNeighbors(room.row, room.column) && this.getRoomNeighbors(room.row, room.column) == 1);
+        if (!rooms.length) {
+          room.setDeadEnd(true);
+          return;
+        }
+        room = getRandomElement(rooms);
         --maxLength;
       }
     }
     createDeadEnd(Math.round(roomsLeft / 3));
+  }
+
+  /** Returns the number of undefined neighbors */
+  public getUndefinedNeighbors(row: number, column: number) {
+    return 4 - Object.keys(this.getNeighbors(row, column)).length;
+  }
+
+  /** Returns the number of non-unknown rooms connected */
+  public getRoomNeighbors(row: number, column: number) {
+    return Object.values(this.getNeighbors(row, column))
+      .reduce((prev, room) => prev + (room.getType() != 'unknown' ? 1 : 0), 0);
   }
 
   private addRoom(row: number, column: number, type: RoomType) {
@@ -143,6 +162,7 @@ export class Floor {
       }
       const newRoom = new Room(room.row - 1, room.column, room.getDistanceToCenter() + 1, false, "unknown");
       this.setRoomAtCoord(newRoom, room.row - 1, room.column);
+      this.roomsArray.push(newRoom);
     }
     if (!neighbors.down) {
       if (this.getRowIndex(room.row + 1) >= this.roomsMatrix.length) {
@@ -150,6 +170,7 @@ export class Floor {
       }
       const newRoom = new Room(room.row + 1, room.column, room.getDistanceToCenter() + 1, false, "unknown");
       this.setRoomAtCoord(newRoom, room.row + 1, room.column);
+      this.roomsArray.push(newRoom);
     }
     if (!neighbors.left) {
       if (this.getColIndex(room.column - 1) < 0) {
@@ -159,6 +180,7 @@ export class Floor {
       }
       const newRoom = new Room(room.row, room.column - 1, room.getDistanceToCenter() + 1, false, "unknown");
       this.setRoomAtCoord(newRoom, room.row, room.column - 1);
+      this.roomsArray.push(newRoom);
     }
     if (!neighbors.right) {
       if (this.getColIndex(room.column + 1) < 0) {
@@ -166,33 +188,29 @@ export class Floor {
       }
       const newRoom = new Room(room.row, room.column + 1, room.getDistanceToCenter() + 1, false, "unknown");
       this.setRoomAtCoord(newRoom, room.row, room.column + 1);
+      this.roomsArray.push(newRoom);
     }
   }
 
-  getNeighbors(row: number, column: number, outOfBoundsCheck = true) {
-    row -= this.offset.row;
-    column -= this.offset.column;
-    const neighbors: Neighbors = {
-      length: 0,
-    };
-    if (outOfBoundsCheck && (row < 0 || row >= this.roomsMatrix.length || column < 0 || column >= this.roomsMatrix[0].length)) {
+  public getNeighbors(row: number, column: number) {
+    row = this.getRowIndex(row);
+    column = this.getColIndex(column);
+    const neighbors: Neighbors = {};
+    if (!this.roomsMatrix[row][column]) {
+      console.error("Invalid coordinates: ", row, column)
       return neighbors;
     }
 
     if (this.roomsMatrix[row - 1] && this.roomsMatrix[row - 1][column]) {
-      ++neighbors.length;
       neighbors.up = this.roomsMatrix[row - 1][column];
     }
     if (this.roomsMatrix[row + 1] && this.roomsMatrix[row + 1][column]) {
-      ++neighbors.length;
       neighbors.down = this.roomsMatrix[row + 1][column];
     }
     if (this.roomsMatrix[row] && this.roomsMatrix[row][column - 1]) {
-      ++neighbors.length;
       neighbors.left = this.roomsMatrix[row][column - 1];
     }
     if (this.roomsMatrix[row] && this.roomsMatrix[row][column + 1]) {
-      ++neighbors.length;
       neighbors.right = this.roomsMatrix[row][column + 1];
     }
     return neighbors;
