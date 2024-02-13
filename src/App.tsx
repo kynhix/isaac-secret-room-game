d6277c61f28d7984f26971eceef3f2191968c807
kynhix <kristopher.t.hicks@gmail.com>
2024-02-12 19:39:35 -0500

    Updated style more

--- src/App.tsx
+++ src/App.tsx
@@ -39,10 +39,13 @@ const App: Component = () => {
   return (
     <div class='bg-neutral-950'>
       <div class='relative flex flex-col min-h-screen h-fit justify-center items-center'>
-        <h1 class='absolute top-20 text-4xl font-bold text-white tracking-tighter'>The Binding of Isaac: <span class='text-blue-400'>Secret Room Game</span></h1>
-        <div class='text-white'>
+        <div class='absolute flex justify-center items-center flex-wrap top-0 pt-10 w-full'>
+          <h1 class='text-center h-fit text-4xl font-bold text-white tracking-tighter'>The Binding of Isaac: <span class='text-blue-400'>Secret Room Game</span></h1>
+          <svg width="64px" height="64px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(4.32,4.32), scale(0.64)"><rect x="-2.4" y="-2.4" width="28.80" height="28.80" rx="14.4" fill="#0000" stroke-width="0"></rect></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 8H12.01M12 11V16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        </div>
+        <div class='text-white pt-28'>
           <For each={floor().getRooms}>{(row) =>
-            <div class='flex flex-row'>
+            <div class='flex flex-row gap-1 mt-1'>
               <For each={row}>{(room) => {
                 return room &&
                   // Rooms are 8:7
